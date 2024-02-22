0eea824f620feb17f66cbd9b2bdc79b7d89bf759
hulesh jangde <63199618+huleshjangde@users.noreply.github.com>
2024-02-22 11:52:06 +0530

    windows error

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import React from "react";
 const page = () => {
   return (
     <div className="flex w-fit h-screen">
-      <HealthOverview />
+      {/* <HealthOverview /> */}
       <Bmi />
     </div>
   );
