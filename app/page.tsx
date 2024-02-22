9b9c0e0e4c0a23e1e9a8246a8ba2b50f8a8371ee
hulesh jangde <63199618+huleshjangde@users.noreply.github.com>
2024-02-22 14:39:27 +0530

    dynamic import

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 "use client";
-import Bmi from "@/components/Bmi";
 import HealthOverview from "@/components/healthoverview/HealthOverview";
 import React from "react";
+import dynamic from "next/dynamic";
+const BmiCalculator = dynamic(() => import("@/components/BmiCalculator"), {
+  ssr: false,
+});
 
 const page = () => {
   return (
     <div className="flex sm:flex-row flex-col w-fit h-full items-center sm:p-0 mt-5 sm:m-0">
       {typeof window !== "undefined" && <HealthOverview />}
-      <Bmi />
+      <BmiCalculator />
     </div>
   );
 };
